f05fb8ad956dea580cb2b225741236f4743ac409
Ivan-Grudev <vanek-stark@yandex.ru>
2021-02-28 21:12:46 +0300

    добавил несколько элементов в П1 с помощью cin

--- Задача 1/main.cpp
+++ Задача 1/main.cpp
@@ -6,16 +6,32 @@ using namespace std;
 
 int main()
 {
+    //создаю пос-ть П1
     vector <int> p(10);
     int r = 0;
     for (int i = 0; i < 10; i++) {
         srand(time(0) * r++ * 7);
         p[i] = rand() % 10 + 1;
     }
+    cout << endl;
 
-    for (int i = 0; i < 10; i++) {
-        cout << p[i] << endl;
+    //Добавляю элементы в П1 через cin
+    cout << "How many new elements do you want to add: ";
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cout << "Input new element: ";
+        cin >> x;
+        p.push_back(x);
     }
+
+    cout << "size =  " << p.size() << endl;
+    for (size_t i = 0; i < p.size(); i++) {
+        cout << i << ": " << p[i] << endl;
+    }
+
 
 
     return 0;
